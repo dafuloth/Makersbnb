913f4d9acd72b8e0d4a8b82951676e9cb2e07ff6
kate102 <kate.morris.new@gmail.com>
2019-03-11 16:37:28 +0000

    Added a create table function for the spaces table

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const pool = new Pool({
   connectionString: connectionString,
 })
 
-const createUsersTable = "CREATE TABLE users(id SERIAL PRIMARY KEY," +
-              "name VARCHAR(20) not null," + 
-              "password VARCHAR(10) not null," +
-              "email VARCHAR(40) not null)";
+const createUsersTable = "CREATE TABLE Users(UserId SERIAL PRIMARY KEY," +
+              "UserName VARCHAR(20) not null," +
+              "UserPassword VARCHAR(10) not null," +
+              "UserEmail VARCHAR(40) not null)";
 
-// const createSpaceTable = "CREATE TABLE users(id SERIAL PRIMARY KEY," +
-//               "name VARCHAR(20) not null," + 
-//               "password VARCHAR(10) not null," +
-//               "email VARCHAR(40) not null)";
+const createSpaceTable = "CREATE TABLE Spaces(SpaceId SERIAL PRIMARY KEY," +
+              "OwnerId int not null," +
+              "SpaceName VARCHAR(20) not null," +
+              "SpaceDescription VARCHAR(50) not null," +
+              "PricePerNight DECIMAL(12,2)," +
+              "OwnerId int FOREIGN KEY REFERENCES Users(UserId)")";
+
 
 pool.query(createUsersTable, () => {
   pool.end()
