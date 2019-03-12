aaae08c4ad4ee3f5088b38f1146139aeb44870d3
kate102 <kate.morris.new@gmail.com>
2019-03-12 16:19:50 +0000

    updated user sql

---
+++ spec/databaseConnection.js
@@ -0,0 +1,6 @@
+const { Pool } = require('pg');
+const connectionString = 'postgres://student:@localhost:5432/teamrandom_bnb';
+
+exports.pool = new Pool({
+  connectionString: connectionString,
+});
