2f31d5a235b929444404eca5b590ba6ec18f627c
Steve Kahn <steve@kahn.org.uk>
2019-03-12 16:14:19 +0000

    Added sign in

--- app.js
+++ app.js
@@ -25,13 +25,32 @@ var path = require('path')
 
 console.log('Anybody there?');
 console.log(__dirname)
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/views/index.html'))
+})
 app.get('/signUp', function(req, res) {
-  res.sendFile(path.join(__dirname + '/signUp.html'));
+  res.sendFile(path.join(__dirname + '/views.signUp.html'));
 });
 
 app.post('/signUp', function(req, res) {
+  if(req.query.username != undefined){
+    db.signUp(req.query.username, req.query.email ,req.query.password)
+  }
   console.log(req.query.username)
-  res.end("hello")
+  res.redirect('/')
+})
+
+app.post('/login', function(req, res){
+  res.send({login: true})
+})
+
+app.post('/addSpace', function(req, res){
+  db.addSpace(
+    req.query.name,
+    req.query.description,
+    req.query.price,
+    req.query.owner)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
