0ce3a6ef8188a2edfc30375a90a84fc183401bc9
David Thai <dafuloth@users.noreply.github.com>
2019-03-14 21:38:42 +0000

    code quality: applied the less drastic eslint recommendations

--- app.js
+++ app.js
@@ -18,73 +18,75 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-var fs = require('fs');
-var http = require('http');
-var router = express.Router();
-var path = require('path')
-var db = require('./src/databaseFunctions')
-var User = require('./src/user');
-var Spaces = require('./src/spaces');
-var space;
-var bnbSpaces;
+const fs = require('fs');
+const http = require('http');
+
+const router = express.Router();
+const path = require('path');
+const db = require('./src/databaseFunctions');
+const User = require('./src/user');
+const Spaces = require('./src/spaces');
+
+let space;
+let bnbSpaces;
 
 console.log('Anybody there?');
-console.log(__dirname)
+console.log(__dirname);
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/views/index.html'))
-})
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+});
 
-app.get('/main.css', function(req, res) {
+app.get('/main.css', function (req, res) {
   res.sendFile(__dirname + '/views/main.css');
 });
 
 
-app.get('/signUp', function(req, res) {
+app.get('/signUp', function (req, res) {
   res.sendFile(path.join(__dirname + '/views/signUp.html'));
 });
 
-app.post('/signUp', function(req, res) {
-  if(req.query.username != undefined){
-    db.signUp(req.query.username, req.query.email ,req.query.password)
+app.post('/signUp', function (req, res) {
+  if (req.query.username !== undefined) {
+    db.signUp(req.query.username, req.query.email, req.query.password);
   }
-})
+});
 
-app.post('/', function(req, res){
-    res.redirect('/')
-})
+app.post('/', function (req, res) {
+  res.redirect('/');
+});
 
-app.post('/login', async function(req, res){
+app.post('/login', async function (req, res) {
   // console.log(db.login(req.query.username, req.query.password))
-  bnbUser = new User()
-  if(await bnbUser.login(req.query.username, req.query.password) == true){
-    res.send({login: true})
-  }else{
-    res.send({login: false})
+  bnbUser = new User();
+  if (await bnbUser.login(req.query.username, req.query.password) === true) {
+    res.send({ login: true });
+  } else {
+    res.send({ login: false });
   }
+});
 
-})
-
-app.post('/addSpace', function(req, res){
+app.post('/addSpace', function (req, res) {
   db.addSpace(
     req.query.name,
     req.query.description,
     req.query.price,
-    req.query.owner)
-})
+    req.query.owner,
+  );
+});
 
-app.get('/getSpaces', async function(req, res) {
-  bnbSpaces = new Spaces()
+app.get('/getSpaces', async function (req, res) {
+  bnbSpaces = new Spaces();
   res.send(await bnbSpaces.getAllSpaces());
 });
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-app.get('/space', function(req, res) {
-  space = req.query.spaceid
-  res.sendFile(path.join(__dirname + '/views/space.html'))
-})
+app.get('/space', function (req, res) {
+  space = req.query.spaceid;
+  res.sendFile(path.join(__dirname + '/views/space.html'));
+});
 
-app.get('/spaceinfo', function(req, res) {
-  console.log("Hello")
-  res.send(bnbSpaces.requestSpace(space))
-})
+app.get('/spaceinfo', function (req, res) {
+  console.log("Hello");
+  res.send(bnbSpaces.requestSpace(space));
+});
