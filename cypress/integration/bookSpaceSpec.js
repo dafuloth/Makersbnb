eb44d67398481886d1eb89174bfb9419158f569f
David Thai <dafuloth@users.noreply.github.com>
2019-03-15 16:13:15 +0000

    extended book space test, with CSS

--- cypress/integration/bookSpaceSpec.js
+++ cypress/integration/bookSpaceSpec.js
@@ -10,20 +10,27 @@ describe('Book Space', function() {
     cy.get('#sign-in').click()
     cy.get("#header").should('contain', 'steve')
 
-    // cy.get('#add-space-input').click()
-    // cy.get('#spaceName').type("Mansion")
-    // cy.get('#spacePrice').type(600.00)
-    // cy.get('#spaceDescription').type("For that once in a lifetime experience!")
-    // cy.get('#submitSpace').click()
+    cy.get('#add-space-input').click()
+    cy.get('#spaceName').type("Mansion")
+    cy.get('#spacePrice').type(600.00)
+    cy.get('#spaceDescription').type("For that once in a lifetime experience!")
+    cy.get('#submitSpace').click()
 
     // cy.get('#spaceName').type("Mansion")
     // cy.get('#spacePrice').type(600.00)
     // cy.get('#spaceDescription').type("For that once in a lifetime experience!")
-   
-    cy.get('#spaceFlat').click()
-    cy.get('#pageTitle').should('contain', 'Flat')
+
+    // old-test
+    // cy.get('#spaceFlat').click()
+    // cy.get('#pageTitle').should('contain', 'Flat')
+    // cy.url().should('contain', '/space?') 
+
+    cy.get('#spaceMansion').click()
     cy.url().should('contain', '/space?') 
+    cy.get('#pageTitle').should('contain', 'Mansion')
 
+    cy.get('#spacesContainer').should('contain', 'For that once in a lifetime experience!')
+    cy.get('#spacesContainer').should('contain', '600.00')
 
   })
 })
